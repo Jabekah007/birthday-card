3a928076e0bb52e66aa4ca9af4bced0b94bd644a
Jabekah007 <70330339+Jabekah007@users.noreply.github.com>
2022-10-16 23:01:29 +0000

    refactor

--- setup/src/data.js
+++ setup/src/data.js
@@ -4,34 +4,34 @@ export default [
     name: 'Bertie Yates',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      'https://media.istockphoto.com/photos/choose-positivity-every-morning-picture-id640307210?k=20&m=640307210&s=612x612&w=0&h=ofW4LHzSJEmSe0ijneQllUu7ghRqCr_DRHNMomMp0YA=',
   },
   {
     id: 2,
     name: 'Hester Hogan',
     age: 32,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29tYW4lMjBzbWlsaW5nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
   },
   {
     id: 3,
     name: 'Larry Little',
     age: 36,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      'https://www1.wdr.de/fernsehen/rockpalast/rockpalast_gentleman100~_v-ARDFotogalerie.jpg',
   },
   {
     id: 4,
     name: 'Sean Walsh',
     age: 34,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeztPhZfzY2J2jSMBvuSNIAE5GDs9TMWOatQ&usqp=CAU',
   },
   {
     id: 5,
     name: 'Lola Gardner',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt9G80opplWqvmucg2OS3wYZYSkKaVEORIbP-io83t6skUJh0ZRyQtZPSBPRc3rWvWIKQ&usqp=CAU',
   },
 ];
